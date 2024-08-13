0d1441cf999542d5c45ad75a0033299781460a23
e11sy <Egoramurin@gmail.com>
2024-08-13 17:33:14 +0300

    renaming

--- packages/helpers/src/index.ts
+++ packages/helpers/src/index.ts
@@ -49,41 +49,6 @@ export function isPrintableKey(keyCode: number): boolean {
 }
 
 /**
- * Debouncing method
- * Call method after passed time
- *
- * Note that this method returns Function and declared variable need to be called
- * @param func - function that we're throttling
- * @param wait - time in milliseconds
- * @param immediate - call now
- * @returns
- */
-export function debounce(func: (...args: unknown[]) => void, wait?: number, immediate?: boolean): () => void {
-  let timeout;
-
-  return (...args: unknown[]): void => {
-    // eslint-disable-next-line @typescript-eslint/no-this-alias
-    const context = this;
-
-    // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
-    const later = () => {
-      timeout = null;
-      if (!immediate) {
-        func.apply(context, args);
-      }
-    };
-
-    const callNow = immediate && !timeout;
-
-    window.clearTimeout(timeout);
-    timeout = window.setTimeout(later, wait);
-    if (callNow) {
-      func.apply(context, args);
-    }
-  };
-}
-
-/**
  * Copies passed text to the clipboard
  * @param text - text to copy
  */
