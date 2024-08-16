ee3df61c583990c895d71f1f3b4cb6a1a1c25abf
e11sy <Egoramurin@gmail.com>
2024-08-16 14:44:02 +0300

    fix fokus method

--- packages/caret/src/focus.ts
+++ packages/caret/src/focus.ts
@@ -4,16 +4,25 @@
  * @param atStart - where to set focus: at the start or at the end
  */
 export function focus(element: HTMLElement, atStart: boolean = true): void {
-  const range = document.createRange();
-  const selection = window.getSelection();
+  if (element instanceof HTMLInputElement || element instanceof HTMLTextAreaElement) {
+    // Если элемент является нативным input или textarea
+    element.focus();
+    const position = atStart ? 0 : element.value.length;
 
-  if (!selection) {
-    return;
+    element.setSelectionRange(position, position);
+  } else {
+    // Если элемент является contenteditable
+    const range = document.createRange();
+    const selection = window.getSelection();
+
+    if (!selection) {
+      return;
+    }
+
+    range.selectNodeContents(element);
+    range.collapse(atStart);
+
+    selection.removeAllRanges();
+    selection.addRange(range);
   }
-
-  range.selectNodeContents(element);
-  range.collapse(atStart);
-
-  selection.removeAllRanges();
-  selection.addRange(range);
 }
